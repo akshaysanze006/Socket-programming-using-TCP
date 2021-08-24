c337815a1402498ef8fb927e7f6cab412863903a
AKSHAY SANZE <60998061+akshaysanze006@users.noreply.github.com>
2021-08-24 12:44:39 +0530

    Create TCP Client.java

---
+++ TCP Client.java
@@ -0,0 +1,60 @@
+import java.io.*; 
+import java.net.*; 
+public class Tcpclient extends Thread 
+{ 
+Thread t1,t2; 
+BufferedReader br,buf; 
+PrintWriter pout; 
+String str,a; 
+Socket client; 
+Tcpclient() 
+{ 
+try 
+{ 
+client=new Socket("127.0.0.1",5555); 
+br=new BufferedReader(new  
+InputStreamReader(System.in)); 
+buf=new BufferedReader(new  
+InputStreamReader(client.getInputStream())); 
+pout=new PrintWriter(client.getOutputStream(),true); 
+} 
+catch(Exception e) 
+{ System.out.println("error :"+e); 
+} 
+} 
+public void run() 
+{ 
+while(true) 
+{ 
+if(Thread.currentThread()==t1) 
+{
+25 
+
+try 
+{ 
+str=br.readLine(); 
+pout.println(str); 
+} 
+catch(Exception e) 
+{ System.out.println("error :"+e); 
+} 
+} 
+else 
+{ 
+try 
+{ 
+a=buf.readLine(); 
+System.out.println("from server :"+a); 
+} 
+catch(Exception e) 
+{ System.out.println("error :"+e); 
+} 
+} 
+} 
+} 
+public static void main(String args[]) 
+{ Tcpclient s=new Tcpclient(); 
+s.t1=new Thread(s); 
+s.t2=new Thread(s); 
+s.t1.start(); 
+s.t2.start();}} 
